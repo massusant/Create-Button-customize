8036c8621f4a697d875438e047f68d3f01956fe3
Dwi Susanto <massusant@gmail.com>
2020-05-19 23:03:50 +0700

    add

---
+++ Create Button/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Create Button
+//
+//  Created by Dwi Susanto on 19/05/20.
+//  Copyright © 2020 Kelana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: {
+            // func here
+        } ){
+            Text("Button")
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .font(.title)
+            .border(Color.black, width: 5)
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
